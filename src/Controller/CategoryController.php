9eb3e8aa1c2ae7f73aec10d205441d710e300932
SauroMatheo <sauromatheo@gmail.com>
2024-11-27 12:15:32 +0100

    Beggining the automatic form for adding a category
    It's still missing the send button and hasn't been tested yet.

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,6 +8,10 @@ use Symfony\Component\Routing\Attribute\Route;
 
 use App\Repository\CategoryRepository;
 
+use App\Form\CategoryType;
+
+use App\Entity\Category;
+
 #[Route('/categories')]
 class CategoryController extends AbstractController
 {
@@ -16,11 +20,41 @@ class CategoryController extends AbstractController
         CategoryRepository $categoryRepo
         ): Response
     {
-        // TODO: La limite est à 4 pour montrer qu'elle fonctionne, modifier plus tard
+        // TODO: Limiting to 4, just to show off that the limit works. Change me !
         $categories = $categoryRepo->findBy([], limit: 4);
 
         return $this->render('category/index.html.twig', [
             'categories' => $categories
+        ]);
+    }
+
+    #[Route('/add', name: 'add_category')]
+    public function add(
+        CategoryRepository $categoryRepo,
+        Request $request,
+        EntityManagerInterface $entityManager
+        ): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            
+
+            $entityManager->persist($categoryRepo);
+            $entityManager->flush();
+
+            /* TODO: Should instead redirect to a detailed view of the category...
+            But a details page with only the name displayed is a bit redundant
+            */ 
+            return $this->redirectToRoute('categories');
+        }
+
+        return $this->render('category/add_form.html.twig', [
+            'form' => $form
         ]);
     }
 }
