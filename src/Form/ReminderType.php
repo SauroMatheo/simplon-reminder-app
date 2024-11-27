1ccc9b973b17afca5be18935726fbf1473bee9c4
SauroMatheo <sauromatheo@gmail.com>
2024-11-27 13:48:33 +0100

    Replaced previous controller with make:crud ones

---
+++ src/Form/ReminderType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Reminder;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReminderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('dueDate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('completedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('isDone')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reminder::class,
+        ]);
+    }
+}
